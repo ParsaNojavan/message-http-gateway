39b4a1217ef402b4bfe4f40b9be20d8e99a0b559
parsanojavan <parsa.nojavan85@gmail.com>
2026-07-26 20:34:18 +0330

    feat(gateway): add user auth endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AppService } from './app.service';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UserModule } from './user/user.module';
+import { JwtStrategy } from '@app/contracts/utils/jwt_token/strategies/jwt.strategy';
 
 @Module({
-  imports: [
-        ClientsModule.register([
-            {
-                name: 'user-client',
-                transport: Transport.REDIS,
-                options: {
-                    host: process.env.REDIS_HOST ?? 'localhost',
-                    port: parseInt(process.env.REDIS_PORT ?? '6379')
-                }
-            }
-        ])
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: {
+                expiresIn: process.env.JWT_EXPIRATION as any
+            },
+            global: true
+        }),
+        UserModule
     ],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [AppController],
+    providers: [AppService,JwtStrategy],
 })
-export class AppModule {}
+export class AppModule { }
